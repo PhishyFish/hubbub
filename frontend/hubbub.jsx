1d42721171e62c7ab4179fa9b7578d10193f6492
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-22 09:31:42 -0800

    Add store

--- frontend/hubbub.jsx
+++ frontend/hubbub.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const root = document.getElementById('root');
+import configureStore from './store/store';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  // For testing purposes:
+  // window.store = store.configureStore;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+
+  const root = document.getElementById('root');
   ReactDOM.render(<h1>Hubbub</h1>, root);
 });
