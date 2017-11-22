589f16ff547b5928777273e7b86427009879a330
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-22 11:59:14 -0800

    Add base CSS, install Font Awesome

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
 const App = () => (
-  <div>
-    <h1>Hubbub</h1>
-    <h2>:)</h2>
+  <div className="container">
+    <header>
+      <h1>Hubbub</h1>
+      <nav>
+        <a href="#" aria-label="Vickie's GitHub">
+          <i className="fa fa-github fa-lg" aria-hidden="true"></i>
+        </a>
+        <a href="#" aria-label="Vickie's LinkedIn">
+          <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
+        </a>
+      </nav>
+    </header>
+    <h1>Join the commotion.</h1>
+    <p>Real-time live chat. No frills. No hassle.</p>
   </div>
 );
 
