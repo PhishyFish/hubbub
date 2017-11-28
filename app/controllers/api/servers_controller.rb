95e5959c8386330eff22c0bba991edc518f5e766
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-28 13:28:21 -0800

    Add servers controller

---
+++ app/controllers/api/servers_controller.rb
@@ -0,0 +1,42 @@
+class Api::ServersController < ApplicationController
+  before_action :require_login
+
+  def index
+    @servers = current_user.servers
+    render :index
+  end
+
+  def show
+    @server = Server.find(params[:id])
+
+    if server.members.include?(current_user)
+      render :show
+    else
+      render json: ["You are not a member of this server"], status: 403
+    end
+  end
+
+  def create
+    @server = Server.new(server_params)
+    @server.owner_id = current_user.id
+
+    if @server.save
+      ServerMember.create(server_id: @server.id, member_id: current_user.id)
+      render :show
+    else
+      render json: @server.errors.full_messages, status: 400
+    end
+  end
+
+  def update
+    @server = Server.find(params[:id])
+    @server.update_attributes(server_params)
+    render :show
+  end
+
+  private
+
+  def server_params
+    params.require(:server).permit(:name, :img_url)
+  end
+end
