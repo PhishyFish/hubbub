2a4615786754abd39aa3f65038a21812a95f8d03
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-28 16:12:13 -0800

    Fix seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "Hubbub", password: "password")
+User.create(username: "Hubbub", password: "password")
 Server.create(name: "Global", owner_id: 1)
 Channel.create(name: "chatter", server_id: 1)
 Channel.create(name: "news", server_id: 1)
