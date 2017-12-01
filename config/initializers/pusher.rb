066b165bf8286c1dd52ed8c3cb983eea8e0e7919
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-01 04:07:36 -0800

    Fix Figaro/Pusher settings

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -3,6 +3,6 @@ require 'pusher'
 Pusher.app_id = ENV["pusher_app_id"]
 Pusher.key    = ENV["pusher_key"]
 Pusher.secret = ENV["pusher_secret"]
-Pusher.cluster = 'us2'
-Pusher.logger = Rails.logger
-Pusher.encrypted = true
+Pusher.cluster = ENV["pusher_cluster"]
+Pusher.logger = ENV["pusher_logger"]
+Pusher.encrypted = ENV["pusher_encrypted"]
