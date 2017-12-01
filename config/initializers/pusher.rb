41680788dd3ea8535adfcb7da7a28384ec9a655c
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-01 03:27:40 -0800

    Reset Figaro key

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,8 +1,8 @@
 require 'pusher'
 
-Pusher.app_id = ENV['438869']
-Pusher.key = ENV['16130caa1c2bfa995eaa']
-Pusher.secret = ENV['9d7b411d4ef4d1b743ae']
+Pusher.app_id = ENV["pusher_app_id"]
+Pusher.key    = ENV["pusher_key"]
+Pusher.secret = ENV["pusher_secret"]
 Pusher.cluster = 'us2'
 Pusher.logger = Rails.logger
 Pusher.encrypted = true
