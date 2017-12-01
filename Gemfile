07f3a2570fe1149c78fa6c1f9719f713c0792eeb
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-11-30 22:05:45 -0800

    Remove logger for Heroku

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 gem 'font-awesome-sass'
 gem 'jquery-rails'
+gem 'pusher'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '438869'
+Pusher.key = 'b202d2334b143cb4823e'
+Pusher.secret = '7d697e97a729767fd018'
+Pusher.cluster = 'us2'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
