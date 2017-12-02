ff134116e9446e50d8021d1821ad6bcd0db687da
Vickie <9098023+PhishyFish@users.noreply.github.com>
2017-12-01 16:05:00 -0800

    Delete testing from window

--- frontend/hubbub.jsx
+++ frontend/hubbub.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './store/store';
 import Root from './components/root';
-import { fetchDirectMessage, fetchDirectMessages } from './util/direct_message_api_util';
-import { createDirectMessage } from './actions/direct_message_actions';
-import { createServer } from './actions/server_actions';
-import { createChannel } from './actions/channel_actions';
-import { createMessage } from './util/message_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store;
@@ -18,17 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   } else {
     store = configureStore();
   }
-
-  // For testing purposes:
-  // window.store = store.configureStore;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.fetchDirectMessage = fetchDirectMessage;
-  window.fetchDirectMessages = fetchDirectMessages;
-  window.createDirectMessage = createDirectMessage;
-  window.createServer = createServer;
-  window.createChannel = createChannel;
-  window.createMessage = createMessage;
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
