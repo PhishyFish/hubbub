0b1fa281a4ba5b93eed74ac6ac665f599ba0c962
Vickie <9098023+PhishyFish@users.noreply.github.com>
2018-05-05 12:08:40 -0700

    Round corners of create channel button

--- frontend/components/modals/channel_modal.jsx
+++ frontend/components/modals/channel_modal.jsx
@@ -43,34 +43,42 @@ class ChannelModal extends React.Component {
     e.preventDefault();
     let { serverId } = this.props.match.params;
 
-    this.props.createChannel({ name: this.state.name }, serverId)
-      .then(() => this.props.fetchChannels(serverId)
-      .then(() => this.props.history.push(
-        `/channels/${serverId}/${this.props.channels[this.props.channels.length - 1].id}`
-    )));
+    this.props
+      .createChannel({ name: this.state.name }, serverId)
+      .then(() =>
+        this.props
+          .fetchChannels(serverId)
+          .then(() =>
+            this.props.history.push(
+              `/channels/${serverId}/${
+                this.props.channels[this.props.channels.length - 1].id
+              }`
+            )
+          )
+      );
 
     this.closeModal(e);
   }
 
   render() {
-    return(
+    return (
       <div className="channel-modal">
         <form className="channel-form">
           <h4>Create Text Channel</h4>
           <h5>Channel Name</h5>
-          <input type="text"
-            autoFocus="true"
+          <input
+            autoFocus
+            type="text"
             className="channel-input"
             onChange={this.updateName}
             value={this.state.name}
           />
           <p className="channel-form-desc">
-            Anyone in this server will have access to read or connect to this channel
+            Anyone in this server will have access to read or connect to this
+            channel
           </p>
           <div className="channel-buttons">
-            <button
-              className="channel-submit"
-              onClick={this.handleSubmit}>
+            <button className="channel-submit" onClick={this.handleSubmit}>
               Create Channel
             </button>
             <button className="channel-cancel" onClick={this.closeModal}>
@@ -78,7 +86,7 @@ class ChannelModal extends React.Component {
             </button>
           </div>
         </form>
-        <div className="modal-backdrop" onClick={this.closeModal}></div>
+        <div className="modal-backdrop" onClick={this.closeModal} />
       </div>
     );
   }
